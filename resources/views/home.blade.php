cb05a3523e3dd6df5c67c60aef2f1663c83bc6f2
Ndi7 <ndigiawa.7@gmail.com>
2024-02-22 06:57:57 +0700

    Update Homecontroller, HomeBlade.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh -->
+    SELAMAT DATANG DI HOME {{$nama}}<br>
+    Anda Adalah Seorang {{$pekerjaan}}
+</div>
