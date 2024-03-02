22872610b3ec09666cf1c173e4b7cec215a7f260
Ndi7 <ndigiawa.7@gmail.com>
2024-03-03 00:03:17 +0700

    buat Carousel Katalog produk (crsl.css dan js)

--- src/crsl.js
+++ src/crsl.js
@@ -1,41 +1,56 @@
 const gallerycrslContainer = document.querySelector('.gallerycrsl-container');
 const gallerycrslControlContainer = document.querySelector('.gallerycrsl-controls');
-const gallerycrslControl = ['previous', 'next'];
-const galleryItems = document.querySelector(.gallery-item);
+const gallerycrslItems = document.querySelectorAll('.gallerycrsl-item');
 
 class Carousel {
 
-    constructor(container, items, controls){
+    constructor(container, items){
         this.carouselContainer = container;
-        this.carouselControls = controls;
         this.carouselArray = [...items];
+        this.autoSlideInterval = null;
+        this.slideIntervalDuration = 3000;
     }
 
     updateGallery(){
         this.carouselArray.forEach(el => {
-            el.classlist.remove('gallery-item-1');
-            el.classlist.remove('gallery-item-2');
-            el.classlist.remove('gallery-item-3');
-            el.classlist.remove('gallery-item-4');
-            el.classlist.remove('gallery-item-5');
+            el.classList.remove('gallerycrsl-item-1', 'gallerycrsl-item-2', 'gallerycrsl-item-3', 'gallerycrsl-item-4', 'gallerycrsl-item-5');
         });
 
-        this.carouselArray.slice(0, 5).forEach(el , i) => {
-            el.classlist.add('gallery-item-${i+1}');
+        this.carouselArray.slice(0, 5).forEach((el, i) => {
+            el.classList.add(`gallerycrsl-item-${i+1}`);
         });
     }
+
     setCurrentState(direction){
-        if (direction.className == 'gallerycrsl-controls-previous'){
-            this.carouselArray.unshift(this.carouselArray.pop());
-        }else{
+        if (direction === 'previous'){
             this.carouselArray.push(this.carouselArray.shift());
+        } else {
+            this.carouselArray.unshift(this.carouselArray.pop());
         }
         this.updateGallery();
     }
 
-    setControls() {
-        this.carouselControls.forEach(control => {
-            gallerycrslControlContainer.appendChild(document.createElement('button'))
-        })
+    startAutoSlide() {
+        this.autoSlideInterval = setInterval(() => {
+            this.setCurrentState('next');
+        }, this.slideIntervalDuration);
+    }
+
+    stopAutoSlide() {
+        clearInterval(this.autoSlideInterval);
+    }
+
+    setAutoSlide() {
+        this.startAutoSlide();
+        this.carouselContainer.addEventListener('mouseenter', () => {
+            this.stopAutoSlide();
+        });
+        this.carouselContainer.addEventListener('mouseleave', () => {
+            this.startAutoSlide();
+        });
     }
 }
+
+const exampleCarousel = new Carousel(gallerycrslContainer, gallerycrslItems);
+
+exampleCarousel.setAutoSlide();
