b71a67756c4794b3f393d27f01b09def6551a790
DwiAulia76 <dwiaulia0764@gmail.com>
2024-03-24 12:06:12 +0700

    tugas pemweb kemaren

---
+++ resources/views/jeni.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('styles/jeni.css') }}">
+    <title>Paktikum 5</title>
+</head>
+<body>
+    <h1>Gambar Pertama</h1>
+    <img src="{{ asset('images/cats.jpeg') }}" alt="Deskripsi Gambar Pertama" width="300">
+    
+    <h1>Gambar Kedua</h1>
+    <img src="{{ asset('images/beach.jpg') }}" alt="Deskripsi Gambar Kedua" width="300">
+</body>
+</html>
