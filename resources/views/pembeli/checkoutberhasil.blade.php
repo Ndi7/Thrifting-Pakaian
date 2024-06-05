0c335833a2a8f707cbb31b876f576d5d21646d54
Ndi7 <ndigiawa.7@gmail.com>
2024-06-05 23:19:21 +0700

    update checklist checkout berhasil

--- resources/views/pembeli/checkoutberhasil.blade.php
+++ resources/views/pembeli/checkoutberhasil.blade.php
@@ -12,9 +12,10 @@
 <body class="bg-gray-100 min-h-screen flex items-center justify-center">
 <div class="container mx-auto bg-white shadow-lg rounded-lg p-8 flex flex-col items-center">
     <div class="text-center">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 mx-auto text-green-500" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M10 2a8 8 0 100 16 8 8 0 000-16zM8.293 9.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 1 1 1.414 1.414l-4 4a1 1 0 01-1.414 0l-2-2a1 1 0 010-1.414z" clip-rule="evenodd" />
-        </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 mx-auto text-green-500" viewBox="0 0 20 20" fill="currentColor">
+    <path fill-rule="evenodd" d="M10 2a8 8 0 100 16 8 8 0 000-16zm-1.5 9.5l4.5-4.5 1.5 1.5-6 6-3-3 1.5-1.5 1.5 1.5z" clip-rule="evenodd"></path>
+    </svg>
+
         <h2 class="text-xl font-semibold mt-4">Checkout Berhasil!!</h2>
         <p class="text-gray-600 mt-2"></p>
         <div class="w-full max-w-lg p-4">
@@ -59,11 +60,11 @@
 
         <div class="mt-8">
             <!-- Tombol untuk membuka modal -->
-            <button id="btnLihatDetail" class="text-indigo-500 font-semibold hover:underline focus:outline-none">Lakukan Pembayaran</button>
+            <button id="btnLihatDetail" class="bg-gray-300 text-gray-700 py-2 px-4 rounded-md hover:bg-gray-400">Lakukan Pembayaran</button>
         </div><br>
-        <div>
+        <!-- <div>
             <a href="../landingpage" class="bg-gray-300 text-gray-700 py-2 px-4 rounded-md hover:bg-gray-400">Kembali ke Home</a>
-        </div>
+        </div> -->
     </div>
 
 
