0ae681a5522931573784049ce5490ad9c56b96be
DwiAulia76 <dwiaulia0764@gmail.com>
2024-06-20 22:30:57 +0700

    buyer profil

--- resources/views/penjual/profile/data-profile.blade.php
+++ resources/views/penjual/profile/data-profile.blade.php
@@ -15,20 +15,20 @@
         </div>
         <div class="-mt-20 flex flex-col items-center">
           {{-- Check if $profil->photo exists --}}
-          @if($profil->photo)
-            <img src="{{ asset('images/profile/Seller/'. $profil->photo) }}" class="w-40 h-40 rounded-full border-4 border-white" />
+          @if($profilpnjl->photo)
+            <img src="{{ asset('images/profile/Seller/'. $profilpnjl->photo) }}" class="w-40 h-40 rounded-full border-4 border-white" />
           @else
             <img src="https://cc64k.aktivin.id/assets/avatar-placeholder-63430c6b.png" class="w-40 rounded-full border-4 border-white" />
           @endif
           <div class="mt-2 flex items-center space-x-2">
-            <p class="text-2xl">{{ $profil->name }}</p>
+            <p class="text-2xl">{{ $profilpnjl->name }}</p>
           </div>
-          <p class="text-gray-700">{{ $profil->email }}</p>
+          <p class="text-gray-700">{{ $profilpnjl->email }}</p>
         </div>
         <div class="mt-2 flex flex-1 flex-col items-center justify-end px-8 lg:items-end">
 
           <div class="mt-2 flex items-center space-x-4">
-            <a href="{{ route('edit.profilpenjual.form', $profil->id) }}">
+            <a href="{{ route('edit.profilpenjual.form', $profilpnjl->id) }}">
               <button class="flex items-center space-x-2 rounded bg-lime-600 px-4 py-2 text-sm text-gray-100 transition duration-100 hover:bg-lime-700">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path>
@@ -48,29 +48,29 @@
               <li class="flex border-y py-2">
                 <span class="w-24 font-bold">Nama</span>
                 <span class=" mx-2 font-bold">:</span>
-                <span class="text-gray-700">{{ $profil->name }}</span>
+                <span class="text-gray-700">{{ $profilpnjl->name }}</span>
               </li>
 
               <li class="flex border-b py-2">
                 <span class="w-24 font-bold">Bergabung</span>
                 <span class=" mx-2 font-bold">:</span>
-                <span class="text-gray-700">{{ $profil->created_at }}</span>
+                <span class="text-gray-700">{{ $profilpnjl->created_at }}</span>
               </li>
 
               <li class="flex border-b py-2">
                 <span class="w-24 font-bold">Lokasi</span>
                 <span class=" mx-2 font-bold">:</span>
-                <span class="text-gray-700">{{ $profil->address }}</span>
+                <span class="text-gray-700">{{ $profilpnjl->address }}</span>
               </li>
               <li class="flex border-b py-2">
                 <span class="w-24 font-bold">No Telepon</span>
                 <span class=" mx-2 font-bold">:</span>
-                <span class="text-gray-700">{{ $profil->phone }}</span>
+                <span class="text-gray-700">{{ $profilpnjl->phone }}</span>
               </li>
               <li class="flex border-b py-2">
                 <span class="w-24 font-bold">Status</span>
                 <span class=" mx-2 font-bold">:</span>
-                <span class="text-gray-700">{{ $profil->level }}</span>
+                <span class="text-gray-700">{{ $profilpnjl->level }}</span>
               </li>
             </ul>
           </div>
@@ -80,4 +80,3 @@
    </div>
 @endsection
 </div>
-    
